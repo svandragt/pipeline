10f83243b7cd22a1459adf169b727fb631d24b5b
Sander van Dragt <sander@vandragt.com>
2018-09-01 12:01:05 +0100

    404 header set when router path not found

--- src/Router.php
+++ src/Router.php
@@ -6,15 +6,18 @@ class Router {
 
 	private $data;
 
-	public function __construct(&$data) {
+	public function __construct( &$data ) {
 		$this->data = $data;
 	}
 
 	public function __destruct() {
-		if (empty($this->routes[$this->path_info])) { 
-			echo '404'; return; 
+		if ( empty( $this->routes[ $this->path_info ] ) ) {
+			header( "HTTP/1.0 404 Not Found" );
+			echo '404 Page not found';
+
+			return;
 		}
 
-		call_user_func_array($this->routes[$this->path_info],array(&$this->data));
+		call_user_func_array( $this->routes[ $this->path_info ], array( &$this->data ) );
 	}
 }
